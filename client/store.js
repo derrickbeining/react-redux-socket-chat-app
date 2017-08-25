bb970ce1476c33f4093ccd079fa03e31066e7dd7
Jing Jia <nycjing@gmail.com>
2017-08-25 10:54:24 -0400

    reduc store

--- client/store.js
+++ client/store.js
@@ -1,10 +1,27 @@
+import {createStore} from 'redux'
 
+const GOT_MESSAGES_FROM_SERVER = 'GOT_MESSAGES_FROM_SERVER';
+const initialState = {
+    messages:[]
+};
 
-const GOT_MESSAGES_FROM_SERVER = 'GOT_MESSAGES_FROM_SERVER'
-
-export const gotMessagesFromServer = function (messages) {
+export function gotMessagesFromServer(messages) {
     return {
         type: GOT_MESSAGES_FROM_SERVER,
-        Messages: messages
+        messages: messages
     };
+
 };
+
+function reducer (state = initialState, action) {
+    switch (action.type){
+        case GOT_MESSAGES_FROM_SERVER:
+            return Object.assign({}, state, {messages:action.messages});
+        default:
+            return state;
+    }
+};
+
+const store = createStore(reducer);
+export default store;
+
