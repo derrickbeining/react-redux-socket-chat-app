aeb42c548f3ceb8a0b8547e0cf6b7d596d706e97
Jing Jia <nycjing@gmail.com>
2017-08-25 10:21:27 -0400

    action creator

--- client/store.js
+++ client/store.js
@@ -2,3 +2,9 @@
 
 const GOT_MESSAGES_FROM_SERVER = 'GOT_MESSAGES_FROM_SERVER'
 
+export const gotMessagesFromServer = function (messages) {
+    return {
+        type: GOT_MESSAGES_FROM_SERVER,
+        Messages: messages
+    };
+};
