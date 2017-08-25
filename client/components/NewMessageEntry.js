afb862d1a37add1e2c5d05a1c8812069b3b6e6e7
Jing Jia <nycjing@gmail.com>
2017-08-25 12:28:09 -0400

    add message to channal

--- client/components/NewMessageEntry.js
+++ client/components/NewMessageEntry.js
@@ -1,16 +1,47 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import store, {writeMessage, receivePostedMessage} from '../store';
 
 export default class NewMessageEntry extends Component {
+    constructor(props) {
+        super();
+        this.state = store.getState();
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    componentDidMount () {
+        this.unsubscribe = store.subscribe(() => this.setState(store.getState()));
+    }
+
+    componentWillUnmount () {
+        this.unsubscribe();
+    }
+
+    handleChange(event){
+        store.dispatch(writeMessage(event.target.value))
+    }
+
+    handleSubmit(evt){
+        evt.preventDefault()
+        axios.post('/api/messages', {content: this.state.newMessageContent , channelId: this.props.channelId})
+            .then(res => res.data)
+            .then(message => store.dispatch(receivePostedMessage(message)))
+            .catch(err=>{
+              console.error(err);
+            });
+    }
 
   render () {
     return (
-      <form id="new-message-form">
+      <form id="new-message-form" onSubmit={this.handleSubmit}>
         <div className="input-group input-group-lg">
           <input
             className="form-control"
             type="text"
             name="content"
-            placeholder="Say something nice..."
+            value = {this.state.newMessageContent}
+            onChange={this.handleChange}
           />
           <span className="input-group-btn">
             <button className="btn btn-default" type="submit">Chat!</button>
--- client/store.js
+++ client/store.js
@@ -1,8 +1,11 @@
 import {createStore} from 'redux'
 
 const GOT_MESSAGES_FROM_SERVER = 'GOT_MESSAGES_FROM_SERVER';
+const RECEIVE_POSTED_MESSAGE = 'RECEIVE_POSTED_MESSAGE';
+const WRITE_MESSAGE = 'WRITE_MESSAGE'
 const initialState = {
-  messages: []
+    newMessageContent: '',
+    messages: []
 };
 
 export function gotMessagesFromServer (messages) {
@@ -10,13 +13,30 @@ export function gotMessagesFromServer (messages) {
     type: GOT_MESSAGES_FROM_SERVER,
     messages: messages
   };
+}
 
+export function writeMessage (content) {
+    return {
+        type: WRITE_MESSAGE,
+        newMessageContent: content
+    };
+}
+
+export function receivePostedMessage (message) {
+    return {
+        type: RECEIVE_POSTED_MESSAGE,
+        message : message
+    };
 }
 
 function reducer (state = initialState, action) {
   switch (action.type) {
     case GOT_MESSAGES_FROM_SERVER:
       return Object.assign({}, state, {messages: action.messages});
+    case WRITE_MESSAGE:
+        return Object.assign({}, state, {newMessageContent: action.newMessageContent});
+    case RECEIVE_POSTED_MESSAGE:
+        return Object.assign({}, state, {messages: state.messages.concat(action.message)});
     default:
       return state;
   }
